b9ca3053afb6209cc80831973371f4495af41dd5
julianbermudez <julianbermudez@eprosima.com>
2019-07-16 11:17:32 +0200

    Refs #6001. Add UCLIENT_PLATFORM_POSIX define and rename previous platform defines.

--- include/uxr/client/transport.h
+++ include/uxr/client/transport.h
@@ -1,4 +1,4 @@
-// Copyright 2018 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,41 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef _UXR_CLIENT_TRANSPORT_H_
-#define _UXR_CLIENT_TRANSPORT_H_
+#ifndef UXR_CLIENT_TRANSPORT_H_
+#define UXR_CLIENT_TRANSPORT_H_
 
 #include <uxr/client/config.h>
 
 #ifdef PROFILE_UDP_TRANSPORT
 #include <uxr/client/profile/transport/udp/udp_transport.h>
-#if defined(PLATFORM_NAME_LINUX)
+#if defined(UCLIENT_PLATFORM_POSIX)
 #include <uxr/client/profile/transport/udp/udp_transport_linux.h>
-#elif defined(PLATFORM_NAME_WINDOWS)
+#elif defined(UCLIENT_PLATFORM_WINDOWS)
 #include <uxr/client/profile/transport/udp/udp_transport_windows.h>
-#elif defined(PLATFORM_NAME_NUTTX)
-#include <uxr/client/profile/transport/udp/udp_transport_linux.h>
 #endif
 #endif //PROFILE_UDP_TRANSPORT
 
 #ifdef PROFILE_TCP_TRANSPORT
 #include <uxr/client/profile/transport/tcp/tcp_transport.h>
-#if defined(PLATFORM_NAME_LINUX)
+#if defined(UCLIENT_PLATFORM_POSIX)
 #include <uxr/client/profile/transport/tcp/tcp_transport_linux.h>
-#elif defined(PLATFORM_NAME_WINDOWS)
+#elif defined(UCLIENT_PLATFORM_WINDOWS)
 #include <uxr/client/profile/transport/tcp/tcp_transport_windows.h>
-#elif defined(PLATFORM_NAME_NUTTX)
-#include <uxr/client/profile/transport/tcp/tcp_transport_linux.h>
 #endif
 #endif //PROFILE_TCP_TRANSPORT
 
 #ifdef PROFILE_SERIAL_TRANSPORT
 #include <uxr/client/profile/transport/serial/serial_transport.h>
-#if defined(PLATFORM_NAME_LINUX)
+#if defined(UCLIENT_PLATFORM_POSIX)
 #include <uxr/client/profile/transport/serial/serial_transport_linux.h>
-#elif defined(PLATFORM_NAME_WINDOWS)
-#elif defined(PLATFORM_NAME_NUTTX)
-#include <uxr/client/profile/transport/serial/serial_transport_linux.h>
+#elif defined(UCLIENT_PLATFORM_WINDOWS)
 #endif
 #endif //PROFILE_SERIAL_TRANSPORT
 
-#endif // _UXR_CLIENT_TRANSPORT_H_
+#endif // UXR_CLIENT_TRANSPORT_H_
